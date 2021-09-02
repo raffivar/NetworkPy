cc35c3ffb546e68f169834d8f1c8b37453632f46
cookienator <rafivar@gmail.com>
2021-09-02 15:28:46 +0300

    created client_server connection

--- unit_2/client_tcp.py
+++ unit_2/client_tcp.py
@@ -2,7 +2,13 @@ import socket
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 my_socket.connect(("127.0.0.1", 8820))
-my_socket.send("hello".encode())
-data = my_socket.recv(1024).decode()
-print("The server sent " + data)
+
+data = ""
+while data != "Bye":
+    msg = input("Please enter your message\n")
+    my_socket.send(msg.encode())
+    data = my_socket.recv(1024).decode()
+    print("The server sent " + data)
+
+print("Closing client socket")
 my_socket.close()
