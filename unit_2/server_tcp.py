f4a512235aa69839c0cd2aec86b5ac90b5b9c19a
Raffi V <rafivar@gmail.com>
2021-09-15 22:31:20 +0300

    implemented name_time_rand exercise

--- unit_2/server_tcp.py
+++ unit_2/server_tcp.py
@@ -1,4 +1,12 @@
+import random
 import socket
+from datetime import datetime
+
+COMMAND_NAME = "NAME"
+COMMAND_TIME = "TIME"
+COMMAND_RAND = "RAND"
+
+SERVER_NAME = "Raffi\'s Server"
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("0.0.0.0", 8820))
@@ -10,13 +18,22 @@ data = ""
 while True:
     data = client_socket.recv(1024).decode()
     print("Client sent: " + data)
-    if data == "Bye":
+    if data == COMMAND_NAME:
+        data = SERVER_NAME
+    elif data == COMMAND_TIME:
+        data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    elif data == COMMAND_RAND:
+        # Includes both ends (1 and 10!!)
+        data = str(random.randint(1, 10))
+    elif data == "Bye":
         data = " "
-    if data == "Quit":
+    elif data == "Quit":
         print("closing client socket now...")
         client_socket.send("Bye".encode())
         break
-    client_socket.send("{}!!!".format(data.upper()).encode())
+    else:
+        data = "Command <{}> unknown".format(data)
+    client_socket.send(data.encode())
 
 client_socket.close()
 server_socket.close()
